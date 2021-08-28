7b1e1c4406884bfe4e1db8d9f2dc2ddcb9f688a2
ttoavina <ttokiniainatoavina4@gmail.com>
2021-08-28 19:58:14 +0300

    Final modification

--- hidder.py
+++ hidder.py
@@ -1,15 +1,17 @@
 from PIL import Image
+from PRFE import *
 
 def bin_to_text(tab):
     return ''.join(chr(int(x,2)) for x in tab)
 
-def decode(image):
+def decode(image,key=""):
     pixels = image.load()
     bin_data = []
     current_string = ""
     iteration = 0
     length = 0
     length_string = ""
+    text_for_debug = ""
     
     for i in range(image.size[0]):
         for j in range(image.size[1]):
@@ -22,38 +24,45 @@ def decode(image):
                     
                 elif pixels[i,j][p]%2==0:
                     current_string += "0"
+                    text_for_debug += "0"
                 else:
                     current_string += "1"
+                    text_for_debug += "1"
                     
-                if len(current_string) == 8:
-                    bin_data.append(current_string)
-                    current_string = ""
+#                 if len(current_string) == 8:
+#                     bin_data.append(current_string)
+#                     current_string = ""
                 iteration +=1
                 
                 if len(length_string)==16:
-                    length = int(length_string,2) +16
+                    length = int(length_string,2)
+                    #print(length)
                     
                 
                 
-                if iteration == length:
-                    print(f"length:{length} and iteration: {iteration}")
-                    return bin_to_text(bin_data)
+                if iteration == length+16:
+                    current_string = decrypter(current_string,key)
+                    return current_string[:len(current_string)-1]
     
 
-def encode(text,image):
+def encode(text,image,key=""):
     
     pixels = image.load()
-    
+    text = text + "x"
     text = ''.join(format(x,'08b') for x in bytes(text,"ascii"))
+    print(f"text len is {len(text)}")
+    text = encrypter(text,key)
     print(f"text len is {len(text)}")
     length = len(text)
     length = format(length,'016b')
     iteration = 0
+   
     text = length+text
+    print(text)
     
     for i in range(image.size[0]):
         for j in range(image.size[1]):
-            
+            print(pixels[i,j])
             tmp = list(pixels[i,j])
             
             for k in range(len(tmp)):
